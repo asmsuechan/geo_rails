0a6c02ea93fb4197d1c58a63b4c4dc45ed4e1124
末永良太 <suenagaryoutaabc@gmail.com>
2016-04-02 19:36:02 +0900

    [modify] option(url) of get_geo() modified

--- lib/geo_api/get_geo.rb
+++ lib/geo_api/get_geo.rb
@@ -1,8 +1,8 @@
 module GeoApi
   module GetGeo
-    #def render_geo(url = self.controller_name.to_s + "/" + self.action_name.to_s)
-    def render_geo(url = {:controller => self.controller_name, :action => self.action_name})
-      render_url = url[:controller] + "/" + url[:action]
+    def render_geo(url = {})
+      render_url = self.controller_name + "/" 
+      render_url += url[:url][:action]  unless url[:url][:action].nil?
       render partial: "/geo/get_geo", locals: {url: render_url}
     end
   end
