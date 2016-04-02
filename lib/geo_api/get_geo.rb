6c0bf8ba58e79237f6fb08947c705ffd872e1c74
末永良太 <suenagaryoutaabc@gmail.com>
2016-04-02 18:59:01 +0900

    [modify] the url format modified

--- lib/geo_api/get_geo.rb
+++ lib/geo_api/get_geo.rb
@@ -1,7 +1,9 @@
 module GeoApi
   module GetGeo
-    def render_geo(url = "/" + self.controller_name.to_s + "/" + self.action_name.to_s)
-      render partial: "/geo/get_geo", locals: {url: url}
+    #def render_geo(url = self.controller_name.to_s + "/" + self.action_name.to_s)
+    def render_geo(url = {:controller => self.controller_name, :action => self.action_name})
+      render_url = url[:controller] + "/" + url[:action]
+      render partial: "/geo/get_geo", locals: {url: render_url}
     end
   end
 end
