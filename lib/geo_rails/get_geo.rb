22e6d095d0ee373fb523ef2159b5bf34e3dcf2d9
末永良太 <suenagaryoutaabc@gmail.com>
2016-04-06 12:17:40 +0900

    added arguments to get_geo()

--- lib/geo_rails/get_geo.rb
+++ lib/geo_rails/get_geo.rb
@@ -1,9 +1,9 @@
 module GeoRails
   module GetGeo
-    def render_geo(url = {})
+    def render_geo(url = {}, high_accuracy = false, timeout = 8000, maximum_age = 2000)
       render_url = self.controller_name + "/" 
       render_url += url[:url][:action] unless url[:url][:action].nil?
-      render partial: "/geo/get_geo", locals: {url: render_url}
+      render partial: "/geo/get_geo", locals: {url: render_url, high_accuracy: high_accuracy, timeout: timeout, maximum_age: maximum_age}
     end
   end
 end
