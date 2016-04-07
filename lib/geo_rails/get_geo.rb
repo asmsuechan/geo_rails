fb346042bdb8977b6af50c07fc1fcea9c4f906ab
末永良太 <suenagaryoutaabc@gmail.com>
2016-04-07 21:44:42 +0900

    added watchPosition()

--- lib/geo_rails/get_geo.rb
+++ lib/geo_rails/get_geo.rb
@@ -1,9 +1,9 @@
 module GeoRails
   module GetGeo
-    def render_geo(url = {}, high_accuracy = false, timeout = 8000, maximum_age = 2000)
+    def render_geo(url = {}, high_accuracy = false, timeout = 8000, maximum_age = 2000, watch = false)
       render_url = self.controller_name + "/" 
       render_url += url[:url][:action] unless url[:url][:action].nil?
-      render partial: "/geo/get_geo", locals: {url: render_url, high_accuracy: high_accuracy, timeout: timeout, maximum_age: maximum_age}
+      render partial: "/geo/get_geo", locals: {url: render_url, high_accuracy: high_accuracy, timeout: timeout, maximum_age: maximum_age, watch: watch}
     end
   end
 end
