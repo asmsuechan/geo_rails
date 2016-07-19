f524fb5f85fd824f236c866a873b141b0df98e06
asmsuechan <suenagaryoutaabc@gmail.com>
2016-07-19 23:11:02 +0900

    Fixes nil call on url[] & ajax post url

--- lib/geo_rails/get_geo.rb
+++ lib/geo_rails/get_geo.rb
@@ -1,8 +1,8 @@
 module GeoRails
   module GetGeo
     def render_geo(high_accuracy: false, timeout: 8000, maximum_age: 2000, watch: false, **url)
-      render_url = self.controller_name + "/" 
-      render_url += url[:url][:action] unless url[:url][:action].nil?
+      render_url = self.controller_name
+      render_url += url[:url][:action] if url[:url].present? && url[:url][:action].present?
       render partial: "/geo/get_geo", locals: {url: render_url, high_accuracy: high_accuracy, timeout: timeout, maximum_age: maximum_age, watch: watch}
     end
   end
